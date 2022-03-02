f6c77ae0595328fba54a537f9b6841b7bfa0e2a9
Scott Walter <scottwalter@Scotts-MacBook-Pro.local>
2022-03-02 08:19:29 -0500

    Inital Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Simple Python Script To Send Emails With Sendgrid
+
+Mass Email With Sendgrid 
+
+This Script iterates through a mysql table then sends an individual HTML Email to each email in the DB
+
+Required:
+Pandas 
+Sendgrid
+Mysql.connector
---
+++ email.py
@@ -0,0 +1,47 @@
+import sendgrid
+from sendgrid.helpers.mail import *
+import pandas as pd
+import mysql.connector
+#Connect to Mysql
+mydb = mysql.connector.connect(
+    host="mysqlServer", user="mysqlUser", password="mysqlUserPsswd", database="mysqlDb"
+)
+connection = mydb.cursor()
+#Select Table with pandas
+frame = pd.read_sql("SELECT * FROM marketinglist", mydb)
+#Select Column Of Emails
+emaily = frame["emaily"]
+#Iterate
+for i in range(len(emaily)):
+    emails = emaily[i]
+    #Open the html file you'd like to send
+    with open(
+        "path-to-html",
+        "r",
+        encoding="utf-8",
+    ) as f:
+        #Reading the html file
+        ht = f.read()
+    sg = sendgrid.SendGridAPIClient(
+        "api-key"
+    )
+    mail = Mail()
+    from_email = Email("from-email")
+    print(emails)
+    to_email = Email(emails)
+    
+    subject = "subject"
+    per = Personalization()
+    mail.from_email = from_email
+    mail.subject = subject
+    html_content = Content("text/html", ht)
+    plain_content = Content("text/plain", "plain-text")
+
+    ###Add plain content first
+    mail.add_content(plain_content)
+    ###Add HTML content next
+    mail.add_content(html_content)
+    per.add_to(to_email)
+    mail.add_personalization(per)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response)
